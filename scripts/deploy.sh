70d262775d031e5b5d298f45ae9f7904aaddac91
Petro Matviichuk <pmatv.90@gmail.com>
2018-09-06 14:57:51 +0300

    Minor changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 #echo $1
 /home/travis/bin/aws --version
-/home/travis/bin/aws ec2 describe-instance-status --region us-west-2
+/home/travis/bin/aws ec2 describe-instances --region us-west-2 --debug
 #aws sts get-caller-identity
