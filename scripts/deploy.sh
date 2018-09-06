f45b25aca9d5693184248733c617a85d3d8a1036
Petro Matviichuk <pmatv.90@gmail.com>
2018-09-06 17:22:22 +0300

    Minor changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 #echo $1
 #env
 mkdir -p ~/.aws
-cp credentials  ~/.aws/credentials
+mv credentials  ~/.aws/credentials
 aws --version
 aws configure list
 aws ec2 describe-instances --region us-west-2
