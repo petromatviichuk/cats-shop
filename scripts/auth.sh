63ea3fd4394ea5d9f7dc5ffe67cdb6abc55edf54
Petro Matviichuk <pmatv.90@gmail.com>
2018-09-06 16:18:08 +0300

    Minor changes

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -4,6 +4,8 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials <<EOF
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
+
+chmod 600 ~/.aws/credentials
