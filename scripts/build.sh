fe42e6ae69fecc317b1b94b50b74b6582c03dc4a
Petro Matviichuk <pmatv.90@gmail.com>
2018-09-07 12:04:14 +0300

    Minor changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,8 @@ SHA=$1
 DOCKER_REGISTRY=$2
 
 echo '{ "insecure-registries" : ["'$DOCKER_REGISTRY'"] }' | sudo tee /etc/docker/daemon.json
-cat /etc/docker/daemon.json
+docker --version
+sudo cat /etc/docker/daemon.json
 sudo service docker restart
 docker build -t $DOCKER_REGISTRY:$SHA .
 docker push $DOCKER_REGISTRY:$SHA
